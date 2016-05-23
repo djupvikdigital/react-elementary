b170eb0d52e4e094d7e0d1f9e8bbfdf7b6ddd53f
Reidar Djupvik <mail@3rdhand.info>
2016-05-23 22:15:54 +0100

    Export all functions as one object.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import mergeProps from 'mergeProps';
+import createElement from './createElement';
+import createFactories from './createFactories';
+import createFactory from './createFactory';
+import elements from './elements';
+import mapIntoArray from './mapIntoArray';
+import mergeProps from './mergeProps';
+import transformProps from './transformProps';
 
-export default { mergeProps };
+export default {
+  createElement,
+  createFactories,
+  createFactory,
+  elements,
+  mapIntoArray,
+  mergeProps,
+  transformProps
+};
