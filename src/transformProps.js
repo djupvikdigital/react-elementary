b029f3b09b50d217efc01ce0bc4c1a672127d09f
Reidar Djupvik <mail@3rdhand.info>
2016-05-23 17:29:56 +0100

    Successfully transform innerHtml to dangerouslySetInnerHtml.

--- src/transformProps.js
+++ src/transformProps.js
@@ -1,3 +1,9 @@
+import { assoc, lens, omit, over, pipe, prop } from 'ramda';
+
 export default function transformProps(props) {
-  
+  const innerHtmlLens = lens(
+    prop('innerHtml'),
+    pipe(assoc('dangerouslySetInnerHtml'), omit('innerHtml'))
+  );
+  return over(innerHtmlLens, value => ({ __html: value }), props);
 }
