8cda3100715a157cc5966a8737ead4765fe1e03e
Reidar Djupvik <mail@3rdhand.info>
2016-05-23 17:51:26 +0100

    Transform props in createElement.

--- src/createElement.js
+++ src/createElement.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import transformProps from './transformProps';
+
 const isProps = input => (
   input !== null && typeof input === 'object' && !React.isValidElement(input)
 );
 
 export default function createElement(type, propsOrNode, ...nodes) {
   const args = isProps(propsOrNode) ?
-    [type, propsOrNode, ...nodes] :
+    [type, transformProps(propsOrNode), ...nodes] :
     [type, {}, propsOrNode, ...nodes];
   return React.createElement.apply(null, args);
 }
