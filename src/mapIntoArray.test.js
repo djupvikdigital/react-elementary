c8c614552a8eed1e33bc6169ffd0eb7541b7c766
Reidar Djupvik <mail@3rdhand.info>
2016-05-23 21:40:40 +0100

    Made a more thorough test for mapIntoArray.

--- src/mapIntoArray.test.js
+++ src/mapIntoArray.test.js
@@ -1,15 +1,16 @@
 import expect from 'expect';
 
 import Immutable from 'immutable';
+import { add, map } from 'ramda';
 
 import mapIntoArray from './mapIntoArray';
 
-const identity = x => x;
-
 describe('mapIntoArray', () => {
   it('takes an immutable collection and a mapper and returns an array', () => {
-    const expected = [1, 2, 3];
-    const actual = mapIntoArray(Immutable.fromJS(expected), identity);
+    const oneTwoThree = [1, 2, 3];
+    const addOne = add(1);
+    const expected = map(addOne, oneTwoThree);
+    const actual = mapIntoArray(Immutable.fromJS(oneTwoThree), addOne);
     expect(actual).toEqual(expected);
   });
 });
