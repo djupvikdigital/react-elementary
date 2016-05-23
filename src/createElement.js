7397991c71956b97964799d8b300efc79fde0086
Reidar Djupvik <mail@3rdhand.info>
2016-05-23 23:05:57 +0100

    Added mapProps higher order function for using custom propsMapper.

--- src/createElement.js
+++ src/createElement.js
@@ -4,13 +4,17 @@ import propsMapper from './propsMapper';
 
 const isNode = input => typeof input == 'string' || React.isValidElement(input);
 
-export default function createElement(type, propsOrNode, ...nodes) {
-  let args;
-  if (isNode(propsOrNode)) {
-    args = [type, {}, propsOrNode, ...nodes];
+export function mapProps(propsMapper) {
+  return function createElement(type, propsOrNode, ...nodes) {
+    let args;
+    if (isNode(propsOrNode)) {
+      args = [type, {}, propsOrNode, ...nodes];
+    }
+    else if (propsOrNode != null) {
+      args = [type, propsMapper(propsOrNode), ...nodes];
+    }
+    return React.createElement.apply(null, args || arguments);
   }
-  else if (propsOrNode != null) {
-    args = [type, propsMapper(propsOrNode), ...nodes];
-  }
-  return React.createElement.apply(null, args || arguments);
 }
+
+export default mapProps(propsMapper);
--- src/createElement.test.js
+++ src/createElement.test.js
@@ -1,8 +1,9 @@
 import expect from 'expect';
 
+import { map, toUpper } from 'ramda';
 import React from 'react';
 
-import createElement from './createElement';
+import createElement, { mapProps } from './createElement';
 
 describe('createElement', () => {
   it('supports omitting props', () => {
@@ -16,6 +17,17 @@ describe('createElement', () => {
   it ('handles undefined props properly', () => {
     const expected = React.createElement('div', void 0);
     const actual = createElement('div', void 0);
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe('mapProps', () => {
+  it('takes a props mapper, and returns a createElement function', () => {
+    const fn = mapProps(map(toUpper));
+    const expected = React.createElement(
+      'div', { className: 'BAR', id: 'FOO' }
+    );
+    const actual = fn('div', { className: 'bar', id: 'foo' });
     expect(actual).toEqual(expected);
   });
 });
