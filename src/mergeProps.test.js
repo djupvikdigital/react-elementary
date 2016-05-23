334263e0e88bb99cb0ca1612e95dda5549625a85
Reidar Djupvik <mail@3rdhand.info>
2016-05-23 16:25:27 +0100

    Test file for mergeProps.

---
+++ src/mergeProps.test.js
@@ -0,0 +1,14 @@
+import expect from 'expect';
+
+import mergeProps from './mergeProps';
+
+describe('mergeProps', () => {
+  it('merges props objects, values in latter object overriding former', () => {
+    const expected = {
+      x: 'foo',
+      y: 'bar',
+      z: 'baz'
+    };
+    const actual = mergeProps({ x: 'foo', z: 'quux' }, { y : 'bar', z: 'baz' });
+  });
+});
